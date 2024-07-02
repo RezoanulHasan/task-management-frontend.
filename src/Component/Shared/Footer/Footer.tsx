a60e027b42bd808934d934e08e1c394c99a7b0d3
RezoanulHasan <rezoanulhasan96@gmail.com>
2024-07-03 03:47:18 +0600

    add footer

--- src/Component/Shared/Footer/Footer.tsx
+++ src/Component/Shared/Footer/Footer.tsx
@@ -1,7 +1,13 @@
 const Footer = () => {
   return (
-    <div>
-      <h1 className="text-4xl">footer</h1>
+    <div className="flex justify-center items-center  text-white  bg-black mt-10 flex-col gap-4 p-4">
+      <h1 className="text-center ">
+        All copy rights reserved by @ Rezoanul Hasan{" "}
+      </h1>
+      <p className="text-center  text-teal-500 font-bold">
+        rezoanulhasan96@gmail.com | +8801734639066
+      </p>
+      <p className="text-center">Bangladesh.</p>
     </div>
   );
 };
