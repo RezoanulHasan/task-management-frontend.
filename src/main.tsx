2a18b3af0ba94a13151c5aaa7ecdd70db038fdbb
RezoanulHasan <rezoanulhasan96@gmail.com>
2024-07-03 02:25:34 +0600

    add helper function

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Error from "./Component/Shared/Error/Error";
-import Home from "./Component/Pages/Home/Home";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.ts";
+import Home from "./Component/Pages/Home/Home.tsx";
 
 //import ProtectedRoute from "./Hooks/ProtectedRoute.tsx";
 const router = createBrowserRouter([
